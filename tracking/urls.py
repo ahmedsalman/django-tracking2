1965ed38d7adb93f0b92484538181a59494d8159
Ahmed Salman <ahmed.salman.noor@gmail.com>
2015-06-30 13:51:21 +0400

    new view added to return message when user par the page view limit

--- tracking/urls.py
+++ tracking/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'tracking.views',
     url(r'^$', 'dashboard', name='tracking-dashboard'),
+    url(r'^show-light-box/$', 'show_light_box', name='show_light_box'),
 )
